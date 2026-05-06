b5f9f7d9588c27a28fb6c6bfeb0303520017c4e6
VYLUX <zaheerdb01@gmail.com>
2026-05-07 00:22:20 +0530

    Updated header mobile view + forgot password flow

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,42 @@
-import styles from"./Header.module.css"
-import LOGO from "../../assests/logo/web_logo.png"
+import styles from "./Header.module.css";
+import LOGO from "../../assests/logo/web_logo.png";
 
 import { Link } from "react-router-dom";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
 
-import { FaShoppingCart, FaUser,FaSearch } from "react-icons/fa";
+export default function Header() {
+  return (
+    <div className={styles.cont}>
 
+      {/* LOGO (TOP) */}
+      <div className={styles.logo}>
+        <img src={LOGO} alt="logo" />
+      </div>
 
-export default function Header(){
+      {/* NAV + ICONS SAME ROW */}
+      <div className={styles.navRow}>
 
-    return(
+        {/* NAVIGATION */}
+        <ul className={styles.navList}>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/myorders">Orders</Link></li>
+          <li><Link to="/contactus">Contact</Link></li>
+        </ul>
 
+        {/* ICONS */}
+        <div className={styles.icons}>
+          <Link to="/cartpage">
+            <FaShoppingCart className={styles.cart} />
+          </Link>
 
-        <div className={styles.cont}>
-
-            <div className={styles.logo}>
-                <img src={LOGO} />
-                
-            </div>
-
-            <div className={styles.nav}>
-
-                <ul>
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/products">Products</Link></li>
-                    <li><Link to="/myorders">My Orders</Link></li>
-                    <li><Link to="/contactus">Contact Us</Link></li>
-                 </ul>
-            </div>
-
-
-            <div  className={styles.right}>
-                
-                <Link to="/cartpage"><FaShoppingCart className={styles.cart} /></Link>
-                <Link to="/profile"><FaUser className={styles.profile} /></Link>
-
-                
-            </div>
-
-
-
+          <Link to="/profile">
+            <FaUser className={styles.profile} />
+          </Link>
         </div>
 
+      </div>
 
-
-    );
+    </div>
+  );
 }
