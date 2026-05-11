76f577aadd0094aec9746b98d56771cde243386a
VYLUX <zaheerdb01@gmail.com>
2026-05-11 12:52:48 +0530

    product page desig  onbile chng

--- src/admin/AdminHome/AdminSidebar.jsx
+++ src/admin/AdminHome/AdminSidebar.jsx
@@ -1,16 +1,117 @@
 import styles from "./AdminSidebar.module.css";
-import {Link} from "react-router-dom"
 
+import {
+  FaChartBar,
+  FaHome,
+  FaBoxOpen,
+  FaEye,
+  FaCog,
+  FaShoppingBag
+} from "react-icons/fa";
+
+import { Link, useLocation } from "react-router-dom";
 
 export default function AdminSidebar() {
+
+  const location = useLocation();
+
+  const menu = [
+    {
+      name: "Dashboard",
+      path: "/admindashboard",
+      icon: <FaChartBar />
+    },
+
+    {
+      name: "Home",
+      path: "/adminhome",
+      icon: <FaHome />
+    },
+
+    {
+      name: "Products",
+      path: "/adminproducts",
+      icon: <FaBoxOpen />
+    },
+
+    {
+      name: "View",
+      path: "/adminviewproducts",
+      icon: <FaEye />
+    },
+
+    {
+      name: "Settings",
+      path: "/adminsettings",
+      icon: <FaCog />
+    },
+
+    {
+      name: "Orders",
+      path: "/adminorder",
+      icon: <FaShoppingBag />
+    }
+  ];
+
   return (
-    <div className={styles.left}>
-     <Link to="/admindashboard"><button>Dashboard Page</button></Link>
-      <Link to="/adminhome"><button>Home Page</button></Link>
-      <Link to="/adminproducts"><button>Product Add Page</button></Link>
-      <Link to="/adminviewproducts"><button>Product View Page</button></Link>
-      <Link to="/adminsettings"><button>Cart Settings</button></Link>
-      <Link to="/adminorder"><button>User Order Page</button></Link>
-    </div>
+    <>
+
+      {/* DESKTOP SIDEBAR */}
+
+      <div className={styles.sidebar}>
+
+        {menu.map((item, index) => (
+
+          <Link
+            key={index}
+            to={item.path}
+            className={`${styles.menuCard} ${
+              location.pathname === item.path
+                ? styles.active
+                : ""
+            }`}
+          >
+
+            <div className={styles.icon}>
+              {item.icon}
+            </div>
+
+            <p>{item.name}</p>
+
+          </Link>
+
+        ))}
+
+      </div>
+
+      {/* MOBILE BOTTOM NAV */}
+
+      <div className={styles.mobileNav}>
+
+        {menu.map((item, index) => (
+
+          <Link
+            key={index}
+            to={item.path}
+            className={`${styles.mobileCard} ${
+              location.pathname === item.path
+                ? styles.mobileActive
+                : ""
+            }`}
+          >
+
+            <div className={styles.mobileIcon}>
+              {item.icon}
+            </div>
+
+            <span>{item.name}</span>
+
+          </Link>
+
+        ))}
+
+      </div>
+
+    </>
   );
 }
